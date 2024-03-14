0f92c7a6e495791c117e4bd9cee7bc843de37e93
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-14 00:57:24 -0400

    Refactor mobile menu styling.

--- src/components/navbar/MobileMenu.jsx
+++ src/components/navbar/MobileMenu.jsx
@@ -7,45 +7,33 @@ const MobileMenu = ({ isMobileMenuOpen, onClickToggleMobileMenu }) => {
     // ===<< Mobile Menu Container >>===
     <div
       id='menu'
-      className={`${showOrHideMenu} absolute md:hidden p-6 rounded-lg bg-shortly-darkViolet left-6 right-6 top-20 z-100`}
+      className={`${showOrHideMenu} flex-col fixed inset-0 z-20 items-center self-end w-full h-full
+                 min-h-screen px-6 py-1 pt-24 pb-4 tracking-widest text-white
+                 uppercase divide-y divide-gray-500 opacity-90 bg-veryDarkBlue`}
     >
       <div
-        className='flex flex-col items-center justify-center w-full 
-                 space-y-6 font-bold text-white rounded-sm'
+        className='w-full py-3 text-center'
+        onClick={onClickToggleMobileMenu}
       >
-        <Link
-          className='w-full text-center'
-          onClick={onClickToggleMobileMenu}
-          href='#'
-        >
-          Features
-        </Link>
-        <Link
-          className='w-full text-center'
-          onClick={onClickToggleMobileMenu}
-          href='#'
-        >
-          Pricing
-        </Link>
-        <Link
-          className='w-full text-center'
-          onClick={onClickToggleMobileMenu}
-          href='#'
-        >
-          Resources
-        </Link>
-        <Link
-          className='w-full pt-6 border-t border-gray-400 text-center'
-          href='#'
-        >
-          Login
-        </Link>
-        <Link
-          className='w-full py-3 text-center rounded-full bg-shortly-cyan'
-          href='#'
-        >
-          Sign Up
-        </Link>
+        <Link href='#features'>Features</Link>
+      </div>
+      <div
+        className='w-full py-3 text-center'
+        onClick={onClickToggleMobileMenu}
+      >
+        <Link href='#download'>Download</Link>
+      </div>
+      <div
+        className='w-full py-3 text-center'
+        onClick={onClickToggleMobileMenu}
+      >
+        <Link href='#faq'>FAQ</Link>
+      </div>
+      <div
+        className='w-full py-3 text-center'
+        onClick={onClickToggleMobileMenu}
+      >
+        <Link href='#login'>Login</Link>
       </div>
     </div>
   );
