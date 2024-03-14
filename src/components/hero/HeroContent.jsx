01911fc6329bd01a49501c091a32c17eeaa19e0a
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-14 14:47:54 -0400

    Add HeroContent component.

---
+++ src/components/hero/HeroContent.jsx
@@ -0,0 +1,18 @@
+const HeroContent = () => {
+  return (
+    <>
+      <h1 className='text-3xl font-semibold text-center lg:text-6xl lg:text-left'>
+        A Simple Bookmark Manager
+      </h1>
+      <p
+        className='max-w-md mx-auto text-lg text-center text-gray-400 lg:text-2xl 
+                       lg:text-left lg:mt-0 lg:mx-0'
+      >
+        A clean and simple interface to organize your favourite websites. Open a
+        new browser tab and see your sites load instantly. Try it for free.
+      </p>
+    </>
+  );
+};
+
+export default HeroContent;
