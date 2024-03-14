67e4b08ffb5f944affcb0f7b3e92d7739eee80df
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-14 14:48:33 -0400

    Add HeroSection component to Home page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import HeroSection from '@/components/hero/HeroSection';
 import NavBar from '@/components/navbar/NavBar';
 export default function Home() {
   return (
     <main>
       <NavBar />
+      <HeroSection />
     </main>
   );
 }
