8ae07effe7a493de3d0e08d1cbef62cdf6d13b52
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-14 00:21:25 -0400

    Add responsive breakpoints, new colors, and custom font family.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,26 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
       },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
+      },
+      backgroundImage: () => ({
+        dots: "url('../images/bg-dots.svg')",
+      }),
     },
   },
   plugins: [],
-}
+};
