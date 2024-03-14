d9669d9b5b61fe6fc49b2d6b4762342fa6c51f2d
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-13 23:09:49 -0400

    Add NavBar component to Home page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
-import Image from 'next/image';
-
+import NavBar from '@/components/NavBar';
 export default function Home() {
   return (
     <main>
-      <h1>Bookmark App</h1>
+      <NavBar />
     </main>
   );
 }
