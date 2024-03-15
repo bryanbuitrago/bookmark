ecbfe7f40fd7ace460d280754889acb834394a99
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 00:36:58 -0400

    Add boxesData.js file with browser data.

---
+++ src/components/download/boxesData.js
@@ -0,0 +1,23 @@
+export const BOXES_DATA = [
+  {
+    browserTitle: 'Add to Chrome',
+    minimumVersion: 'Minimum Version 62',
+    imgSrc: '/images/logo-chrome.svg',
+    alt: 'Chrome Logo',
+    addMarginTop: '',
+  },
+  {
+    browserTitle: 'Add to Firefox',
+    minimumVersion: 'Minimum Version 55',
+    imgSrc: '/images/logo-firefox.svg',
+    alt: 'Firefox Logo',
+    addMarginTop: 'md:mt-8',
+  },
+  {
+    browserTitle: 'Add to Opera',
+    minimumVersion: 'Minimum Version 46',
+    imgSrc: '/images/logo-opera.svg',
+    alt: 'Opera Logo',
+    addMarginTop: 'md:mt-16',
+  },
+];
