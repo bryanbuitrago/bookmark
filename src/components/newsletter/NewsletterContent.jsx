70196dafc2eb0c0e6b562be99576bf92cd6d47a0
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 01:05:19 -0400

    Add NewsletterContent component.

---
+++ src/components/newsletter/NewsletterContent.jsx
@@ -0,0 +1,14 @@
+const NewsletterContent = () => {
+  return (
+    <>
+      <p className='mb-6 text-lg tracking-widest text-center text-white uppercase'>
+        35,000+ Already Joined
+      </p>
+      <h2 className='px-3 mb-6 text-3xl font-semibold text-center text-white md:text-4xl'>
+        Stay up-to-date with what we're doing
+      </h2>
+    </>
+  );
+};
+
+export default NewsletterContent;
