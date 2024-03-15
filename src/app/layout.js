830f8a4b1235210890d86d02dd9cebf49a8ba871
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-14 23:50:47 -0400

    Update font and body.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google';
+import { Rubik } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const rubik = Rubik({ subsets: ['latin'], sans: ['Rubik', 'sans-serif'] });
 
 export const metadata = {
   title: 'Bookmark Landing Page',
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={`${rubik.className} overflow-x-hidden`}>{children}</body>
     </html>
   );
 }
