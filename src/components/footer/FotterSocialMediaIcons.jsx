fa00ea71c2aed0f01b4ea1760a987b235177b9d9
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 01:25:36 -0400

    Add FooterSocialMediaIcons component.

---
+++ src/components/footer/FotterSocialMediaIcons.jsx
@@ -0,0 +1,26 @@
+const FotterSocialMediaIcons = () => {
+  return (
+    <>
+      <Link href='#'>
+        <Image
+          src='/bookmark-web-app/images/icon-facebook.svg'
+          alt='Facebook'
+          width={24}
+          height={24}
+          className='h-6 ficon'
+        />
+      </Link>
+      <Link href='#'>
+        <Image
+          src='/bookmark-web-app/images/icon-twitter.svg'
+          alt='Twitter'
+          width={24}
+          height={24}
+          className='h-6 ficon'
+        />
+      </Link>
+    </>
+  );
+};
+
+export default FotterSocialMediaIcons;
