98d26f35511004e76c26ded06e2092e2aa71d7c8
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 00:32:19 -0400

    Add featuresData.js file with initial data.

---
+++ src/components/features/featuresData.js
@@ -0,0 +1,29 @@
+export const TABS_DATA = [
+  { tabID: 'tab-1', title: 'Simple Bookmarking' },
+  { tabID: 'tab-2', title: 'Speedy Searching' },
+  { tabID: 'tab-3', title: 'Easy Sharing' },
+];
+
+export const PANELS_DATA = [
+  {
+    panelID: 'panel-1',
+    title: 'Bookmark in one click',
+    imgSrc: '/images/illustration-features-tab-1.svg',
+    description:
+      'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
+  },
+  {
+    panelID: 'panel-2',
+    title: 'Intelligent search',
+    imgSrc: '/images/illustration-features-tab-2.svg',
+    description:
+      'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
+  },
+  {
+    panelID: 'panel-3',
+    title: 'Easy Sharing',
+    imgSrc: '/images/illustration-features-tab-3.svg',
+    description:
+      'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
+  },
+];
