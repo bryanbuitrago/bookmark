daf398a0dd825c84d8cfb545b6f7d81e2a4cff2e
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 01:23:34 -0400

    Refactor hamburger button styles.

--- src/components/navbar/HamburgerButton.jsx
+++ src/components/navbar/HamburgerButton.jsx
@@ -1,28 +1,20 @@
 const HamburgerButton = ({ isMobileMenuOpen, onClickToggleMobileMenu }) => {
-  const topBarAnimation = isMobileMenuOpen
-    ? 'bookmark-animate-hamburger-top'
-    : '';
-  const middleBarVisibility = isMobileMenuOpen
-    ? 'bookmark-hide-hamburger-middle'
-    : '';
+  const topBarAnimation = isMobileMenuOpen ? 'animate-hamburger-top' : '';
+  const middleBarVisibility = isMobileMenuOpen ? 'hide-hamburger-middle' : '';
   const bottomBarAnimation = isMobileMenuOpen
-    ? 'bookmark-animate-hamburger-bottom'
+    ? 'animate-hamburger-bottom '
     : '';
   return (
     <div className='md:hidden fixed top-14 right-8 z-50'>
       <button
         id='menu-btn'
-        className='block focus:outline-none bookmark-hamburger'
+        className='block focus:outline-none hamburger'
         onClick={onClickToggleMobileMenu}
         type='button'
       >
-        <span className={`${topBarAnimation} bookmark-hamburger-top`}></span>
-        <span
-          className={`${middleBarVisibility} bookmark-hamburger-middle`}
-        ></span>
-        <span
-          className={`${bottomBarAnimation} bookmark-hamburger-bottom`}
-        ></span>
+        <span className={`${topBarAnimation} hamburger-top`}></span>
+        <span className={`${middleBarVisibility} hamburger-middle`}></span>
+        <span className={`${bottomBarAnimation} hamburger-bottom`}></span>
       </button>
     </div>
   );
