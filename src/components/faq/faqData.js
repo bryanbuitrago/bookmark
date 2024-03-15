cb9ed231b40b420561a1520d9c1dbfe1ec8c2a2a
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 00:54:57 -0400

    Add FAQ data.

---
+++ src/components/faq/faqData.js
@@ -0,0 +1,22 @@
+export const FAQ_DATA = [
+  {
+    title: 'What is Bookmark?',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, repellat amet doloribus consequuntur eos similique provident tempora voluptates iure quia fuga dicta voluptatibus culpa mollitia recusandae delectus id suscipit labore?',
+  },
+  {
+    title: 'How can I request a new browser?',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, repellat amet doloribus consequuntur eos similique provident tempora voluptates iure quia fuga dicta voluptatibus culpa mollitia recusandae delectus id suscipit labore?',
+  },
+  {
+    title: 'Is ther a mobile app?',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, repellat amet doloribus consequuntur eos similique provident tempora voluptates iure quia fuga dicta voluptatibus culpa mollitia recusandae delectus id suscipit labore?',
+  },
+  {
+    title: 'What about other Chromium browsers',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, repellat amet doloribus consequuntur eos similique provident tempora voluptates iure quia fuga dicta voluptatibus culpa mollitia recusandae delectus id suscipit labore?',
+  },
+];
