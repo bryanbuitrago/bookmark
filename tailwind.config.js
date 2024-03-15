245de1a8fa5c322a9343c069526c2e9c0a351456
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 00:15:56 -0400

    Update colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        softBlue: 'hsl(231, 69%, 60%)',
         softRed: 'hsl(0, 94%, 66%)',
+        softBlue: 'hsl(231, 69%, 60%)',
         grayishBlue: 'hsl(229, 8%, 60%)',
         veryDarkBlue: 'hsl(229, 31%, 21%)',
       },
