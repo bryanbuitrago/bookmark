d54e7039503c94c242baa06d051ba4bfd04d6f58
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 00:56:06 -0400

    Add ArrowSVGfaq component.

---
+++ src/components/faq/ArrowSVGfaq.jsx
@@ -0,0 +1,14 @@
+const ArrowSVGfaq = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='18' height='12'>
+      <path
+        fill='none'
+        stroke='currentColor'
+        strokeWidth='3'
+        d='M1 1l8 8 8-8'
+      />
+    </svg>
+  );
+};
+
+export default ArrowSVGfaq;
